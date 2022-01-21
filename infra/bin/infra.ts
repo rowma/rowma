3f77f3532a8d9bde13ccaa5765688be38e165654
asmsuechan <suenagaryoutaabc@gmail.com>
2022-01-21 09:28:29 +0900

    Remove cdk v1 libraries

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { App } from '@aws-cdk/core';
 import { InfraStack } from '../lib/infra-stack';
 
-const app = new App();
+const app = new cdk.App();
 new InfraStack(app, 'InfraStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,7 +1,10 @@
-import { Stack, StackProps,Construct } from '@aws-cdk/core';
+import { Construct } from 'constructs'
 
-import { ContainerImage } from '@aws-cdk/aws-ecs';
-import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns';
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import { ApplicationLoadBalancer } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { ContainerImage } from 'aws-cdk-lib/aws-ecs'
+import { ApplicationLoadBalancedFargateService} from 'aws-cdk-lib/aws-ecs-patterns'
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -11,12 +14,19 @@ export class InfraStack extends Stack {
   }
 
   private defineAlbFargateService(scope: Construct) {
+    const vpc = new Vpc(scope, 'VPC');
+    const lb = new ApplicationLoadBalancer(scope, 'lb', { vpc });
+    const listener = lb.addListener('listener', { port: 80 });
+    listener.addTargets('target', {
+      port: 80,
+    });
+
     new ApplicationLoadBalancedFargateService(scope, 'RowmaConnectionManager', {
       taskImageOptions: {
         image: ContainerImage.fromRegistry('rowma/rowma'),
       },
       publicLoadBalancer: true,
-      // loadBalancer: undefined
+      loadBalancer: lb
     });
   }
 }
