419e2f6547c47890ffcd5caa8101b779dbf37c67
asmsuechan <suenagaryoutaabc@gmail.com>
2022-01-22 23:46:51 +0900

    Make infra work

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,38 +1,30 @@
 import { Construct } from 'constructs'
 
 import { Stack, StackProps, Duration } from 'aws-cdk-lib'
-import { Vpc } from 'aws-cdk-lib/aws-ec2';
-import { ApplicationLoadBalancer } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { ContainerImage } from 'aws-cdk-lib/aws-ecs'
-import { ApplicationLoadBalancedFargateService} from 'aws-cdk-lib/aws-ecs-patterns'
+import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns'
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    this.defineAlbFargateService(this)
+    const apiService = this.defineAlbFargateService(this)
+    apiService.targetGroup.enableCookieStickiness(Duration.days(1))
   }
 
-  private defineAlb(scope: Construct) {
-    const vpc = new Vpc(scope, 'VPC');
-    const lb = new ApplicationLoadBalancer(scope, 'lb', { vpc });
-    const listener = lb.addListener('listener', { port: 80 });
-    listener.addTargets('target', {
-      port: 80,
-      stickinessCookieDuration: Duration.days(1),
-    });
-    return lb
-  }
-
-  private defineAlbFargateService(scope: Construct) {
-    const lb = this.defineAlb(scope)
-
-    new ApplicationLoadBalancedFargateService(scope, 'RowmaConnectionManager', {
+  private defineAlbFargateService(scope: Construct): ApplicationLoadBalancedFargateService {
+    return new ApplicationLoadBalancedFargateService(scope, 'RowmaConnectionManager', {
       taskImageOptions: {
-        image: ContainerImage.fromRegistry('rowma/rowma'),
+        image: ContainerImage.fromRegistry("rowma/rowma"),
+        containerPort: 3000,
+        enableLogging: true,
       },
+      memoryLimitMiB: 512,
+      desiredCount: 1,
+      cpu: 256,
+      assignPublicIp: true,
       publicLoadBalancer: true,
-      loadBalancer: lb
+      listenerPort: 80,
     });
   }
 }
